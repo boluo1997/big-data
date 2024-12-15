64b49719913ae1bb8bd03e4918c9592a41063039
dingc <1109928059@qq.com>
2024-12-15 20:46:48 +0800

    创建子项目

---
+++ data-mock/src/main/java/com/boluo/Application.java
@@ -0,0 +1,14 @@
+package com.boluo;
+
+/**
+ * @author chao
+ * @datetime 2024-12-15 20:34
+ * @description
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
