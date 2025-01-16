9000ce0aac4f4e378cb233451a796774035c7bee
dingc <1109928059@qq.com>
2025-01-16 16:05:23 +0800

    data mock 框架完成

---
+++ data-mock/sqls/bronze/api_tables.sql
@@ -0,0 +1,20 @@
+-- 历史上的今天
+CREATE TABLE api_today_on_history(
+    id bigint(20) not null auto_increment primary key,
+    raw_msg varchar(5000),
+    batch_id varchar(5000),
+    load_date date,
+    created_timestamp timestamp
+);
+
+
+-- 万年历
+CREATE TABLE api_calendar(
+    id bigint(20) not null auto_increment primary key,
+    raw_msg varchar(5000),
+    batch_id varchar(5000),
+    load_date date,
+    created_timestamp timestamp
+);
+
+
