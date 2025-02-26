debccbad72c0572fbba5e58b2da60619a53b3be5
dingc <1109928059@qq.com>
2025-02-26 21:40:21 +0800

    写入bronze数据到Mysql

--- data-streaming-ingestion/src/main/java/com/boluo/dao/WriteDaoImpl.java
+++ data-streaming-ingestion/src/main/java/com/boluo/dao/WriteDaoImpl.java
@@ -34,19 +34,33 @@ public class WriteDaoImpl {
                 .foreachBatch((batchDs, batchId) -> {
                     System.out.println("batchId" + batchId);
                     if (!batchDs.isEmpty()) {
-                        batchDs.show(false);
+                        // batchDs.show(false);
+                        String jdbcUrl = "jdbc:mysql://mysql.sqlpub.com:3306/dingchao_db?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai";
+                        String tableName = "bronze_trade";
+                        String userName = "dingchao";
+                        String password = "6049773e1914457d";
+                        SparkUtils.writeToMySQL(batchDs, jdbcUrl, tableName, userName, password);
                     }
                 }).start().awaitTermination();
+
+        // +----------------+---------------+-----------+----------+--------------+
+        // |kafka_offset_num|kafka_partition|kafka_topic|raw_msg   |raw_msg_schema|
+        // +----------------+---------------+-----------+----------+--------------+
+        // |30              |0              |test       |Message: 0|null          |
+        // |31              |0              |test       |Message: 1|null          |
+        // |32              |0              |test       |Message: 2|null          |
+        // |33              |0              |test       |Message: 3|null          |
+        // |34              |0              |test       |Message: 4|null          |
+        // +----------------+---------------+-----------+----------+--------------+
 
     }
 
     public static Dataset<Row> processJsonData(Dataset<Row> ds) {
         return ds.selectExpr(
-                "cast(offset as string) as kafka_offset_num",
-                "cast(partition as string) as kafka_partition",
                 "cast(topic as string) as kafka_topic",
-                "cast(value as string) as raw_msg",
-                "cast(null as string) as raw_msg_schema"
+                "cast(partition as bigint) as kafka_partition",
+                "cast(offset as bigint) as kafka_offset",
+                "cast(value as string) as kafka_raw_msg"
         );
     }
 
